944d1fd276dd8abf2d2852b379b9afec01b1ff11
Anthony Tsang <atsang@anydesk.com>
2024-04-29 10:46:58 +0200

    change settings to meet my current projects

--- lua/heiyiu/plugins/treesitter.lua
+++ lua/heiyiu/plugins/treesitter.lua
@@ -8,7 +8,6 @@ return {
           "c",
           "cpp",
           "rust",
-          "javascript", "typescript",
           "lua",
           "vim", "vimdoc",
           "query",
@@ -22,10 +21,10 @@ return {
     
       -- Automatically install missing parsers when entering buffer
       -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-      auto_install = true,
+      auto_install = false,
     
       -- List of parsers to ignore installing (or "all")
-      -- ignore_install = { "javascript" },
+      ignore_install = { "javascript", "python" },
     
       ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
       -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
