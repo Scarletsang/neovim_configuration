a21d4218668c45473cd34fbca73445e1a0dcb056
Anthony Tsang <anthonytsang0226@gmail.com>
2024-05-01 21:26:42 +0200

    Add keymap for vertical split screen and close screen

--- lua/heiyiu/remap.lua
+++ lua/heiyiu/remap.lua
@@ -2,3 +2,6 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 vim.keymap.set("n", "<leader>h", ":Ex<CR>")
+vim.keymap.set("n", "<leader>s", ":vsplit<CR>")
+vim.keymap.set("n", "<leader>c", ":close<CR>")
+-- Control W + hjkl to move between windows
