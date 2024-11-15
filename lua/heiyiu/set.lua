6a8963a43fb6dabf11c6ccde2e65590495a5525f
Anthony Tsang <atsang@anydesk.com>
2024-11-15 15:51:18 +0100

    show tab as 2 spaces

--- lua/heiyiu/set.lua
+++ lua/heiyiu/set.lua
@@ -3,9 +3,9 @@ vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = false
 vim.opt.listchars = "tab:. ,trail:~,extends:>,precedes:<"
 vim.opt.list = true
