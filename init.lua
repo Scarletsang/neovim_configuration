d556cd7a9e6a3194b8f262b7ab4fccf66efb5143
Anthony Tsang <tsang.keep.art.in.stock@gmail.com>
2025-04-07 04:22:26 +0200

    use coq

--- init.lua
+++ init.lua
@@ -42,6 +42,10 @@ vim.opt.colorcolumn = "80"
 ---- keymap ----
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.g.coq_settings = {
+  auto_start = 'shut-up',
+  ['display.icons.mode'] = 'none',
+}
 
 vim.keymap.set("n", "<leader>h", ":Ex<CR>")
 vim.keymap.set('t', '<C-c>', [[<C-c><C-\><C-n>:close<CR>]])
@@ -111,106 +115,11 @@ require('lazy').setup({
         vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
       end
     },
-    {'saghen/blink.cmp',
-      -- optional: provides snippets for the snippet source
-      dependencies = { 'rafamadriz/friendly-snippets' },
-
-      -- use a release tag to download pre-built binaries
-      version = '1.*',
-      -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-      -- build = 'cargo build --release',
-      -- If you use nix, you can build from source using latest nightly rust with:
-      -- build = 'nix run .#build-plugin',
-
-      ---@module 'blink.cmp'
-      ---@type blink.cmp.Config
-      opts = {
-        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-        -- 'super-tab' for mappings similar to vscode (tab to accept)
-        -- 'enter' for enter to accept
-        -- 'none' for no mappings
-        --
-        -- All presets have the following mappings:
-        -- C-space: Open menu or open docs if already open
-        -- C-n/C-p or Up/Down: Select next/previous item
-        -- C-e: Hide menu
-        -- C-k: Toggle signature help (if signature.enabled = true)
-        --
-        -- See :h blink-cmp-config-keymap for defining your own keymap
-        keymap = {
-          preset = 'default',
-          ['<Tab>'] = { 'select_next', 'fallback' },
-          ['<S-Tab>'] = { 'select_prev', 'fallback' },
-          ['<CR>'] = { 'accept', 'fallback' },
-        },
-
-        appearance = {
-          -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-          -- Adjusts spacing to ensure icons are aligned
-          nerd_font_variant = 'mono',
-          kind_icons = {
-            Text = 'txt',
-            Method = 'meth',
-            Function = 'func',
-            Constructor = 'init',
-
-            Field = '->',
-            Variable = 'var',
-            Property = '->',
-
-            Class = 'class',
-            Interface = 'i/f',
-            Struct = 'struct',
-            Module = 'module',
-
-            Unit = 'unit',
-            Value = 'val',
-            Enum = 'enum',
-            EnumMember = 'enumm',
-
-            Keyword = 'keyword',
-            Constant = 'const',
-
-            Snippet = 'sn',
-            Color = 'col',
-            File = 'file',
-            Reference = 'ref',
-            Folder = 'dir',
-            Event = 'event',
-            Operator = 'op',
-            TypeParameter = 'type',
-          },
-        },
-        signature = { enabled = true },
-
-        -- (Default) Only show the documentation popup when manually triggered
-        completion = {
-          documentation = { auto_show = false },
-          menu = { draw = {
-            treesitter = {'lsp'},
-          }},
-        },
-
-        -- Default list of enabled providers defined so that you can extend it
-        -- elsewhere in your config, without redefining it, due to `opts_extend`
-        sources = {
-          default = { 'lsp', 'path', 'snippets', 'buffer' },
-        },
-
-        -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-        -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-        -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-        --
-        -- See the fuzzy documentation for more information
-        fuzzy = { implementation = "prefer_rust_with_warning" }
-      },
-      opts_extend = { "sources.default" }
-    },
     {'neovim/nvim-lspconfig',
       dependencies = {
         'williamboman/mason.nvim',
         'williamboman/mason-lspconfig.nvim',
-        'saghen/blink.cmp',
+        { 'ms-jpq/coq_nvim', branch = 'coq'},
         'j-hui/fidget.nvim',
       },
       config = function()
@@ -219,7 +128,7 @@ require('lazy').setup({
           ensure_installed = {"clangd"}
         })
         require("fidget").setup({})
-        local capabilities = require('blink.cmp').get_lsp_capabilities()
+        local capabilities = require('coq').lsp_ensure_capabilities()
         local lspconfig = require('lspconfig')
         lspconfig['clangd'].setup({ capabilities = capabilities })
         lspconfig['ols'].setup({ capabilities = capabilities })
